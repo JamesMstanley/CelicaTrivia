f24335453e44cdaff8b120232757e8f970d615fe
James <54610280+JamesMstanley@users.noreply.github.com>
2019-09-17 17:39:21 -0500

    Add files via upload

---
+++ Index.hyperesources/CelicaTrivia.js
@@ -0,0 +1,79 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"CELICA TRIVIA";
+	window.questions = [		"What year was the celica discontinued", 
+								"What year did the Celica come out",
+								"Who makes the Celica", 
+								"Why was the Celica discontinued", 
+								"How many stock colors did the Celica come in from 2000-2006",
+								"What does the word Celica mean",
+							    "What is the origin of the word Celica",
+							    "How many doors were on the first Toyota Celica",
+							    "What engine is in the 2000-2006 Celica",
+							    "Is the Toyota Celica a cool car"];
+	window.correctAnswer = [ 	"A", "C", "B", "E", "D", "B", "E", "C", "A", "D"]; 
+	window.answerA = [ 			"2006", 
+								"1996", 
+								"Ford", 
+								"Reliability issues", 
+								"5", 
+								"Fast as light",
+								"Greek",
+								"3",
+								"1.8L 4-cylinder",
+								"Nah"];
+	window.answerB = [ 			"2000", 
+								"1980", 
+								"Toyota", 
+								"Toyota did not like sports cars",
+								"6", 
+								"Heavenly",
+								"Unknown",
+								"4",
+								"2.8L 4-cylinder",
+								"No"];
+	window.answerC = [ 			"1996",
+								"1970",
+								"Honda", 
+								"The Celica was too expensive", 
+								"10", 
+								"Old",
+								"American",
+								"2",
+								"2.5L 6-cylinder",
+								"Nope"];
+	window.answerD = [ 			"2005",  
+								"1960",
+								"Subaru", 
+								"No reason",
+								"11", 
+								"Rustic",
+								"Japanese",
+								"2.5",
+								"2.2L 4-cylinder",
+								"Yes"];
+	window.answerE = [ 			"2008", 
+								"1965", 
+								"Acura", 
+								"Sales were too low", 
+								"18", 
+								"Metal",
+								"Latin",
+								"1",
+								"1.5L 4-cylinder",
+								"Not really"];
+	window.response = [ 		"The Celica was discontinued in 2006 and Toyota began making more SUV's", 
+								"The Celica came out in 1970 at first as a RWD car then, in 1985 was switched to FWD", 
+								"Toyota made the Celica as a cut down version of the 2000GT", 
+								"Toyota decided to discontinue the Celica in 2006 and make vehicles like the FJ Cruiser and the the Corolla E140", 
+								"The Celica came in 11 colors including yellow, blue, black, and silver" ,
+								"Celica translates to heavenly because of look of the celica to many was very heavenly",
+								"Celica is Latin word meaning celestial or heavenly",
+								"The Toyota Celica was a coupe style car with two doors and a trunk",
+								"The Toyota Celica came with a 1.8 Liter four cylinder engine in the 2000's",
+								"The Celica is pretty awesome"];
+							
+}
+						
